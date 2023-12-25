0fe8af893c94fe548f98a46172ef71fda06add38
Jotiraj Patil <51956131+smita-dev@users.noreply.github.com>
2023-12-25 19:01:04 +0530

    learned spring core

---
+++ learn-spring-boot/src/main/java/com/gameProject/learnspringboot/examples/xmlConfiguration/SimpleSpringScopeApplication.java
@@ -0,0 +1,29 @@
+package com.gameProject.learnspringboot.examples.xmlConfiguration;
+
+import java.util.Arrays;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+
+//@Configuration
+//@ComponentScan
+public class SimpleSpringScopeApplication {
+
+	public static void main(String[] args) {
+		// launch application context
+
+		try (var context=new ClassPathXmlApplicationContext("ContextConfiguration.xml") ){
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+			
+//			context.getBea
+		}
+	}
+
+}
